8ad78960091ed9d3a906fe102069031650ad4a5a
Chris Alan Williams <chris@enactdevelopment.com>
2018-04-24 20:29:52 -0400

    Print 2017 txt file name

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-from .app_config import DB_CONN
+from app_config import DB_CONN
 
 engine = create_engine(DB_CONN)
 
--- import_db.py
+++ import_db.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python
+# coding: utf-8
+import os
+import sys
+import csv
+
+from datetime import datetime
+
 import sqlalchemy as sa
 from database import db_session
-from models import *
+from models import Candidate
 
-import csv
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+year = int(sys.argv[1]) if len(sys.argv) > 1 else datetime.now().year
+
+file_location = current_dir+'/Explorer.Transactions.'+str(year)+'.YTD.txt'
+
+print(file_location)
+
 
